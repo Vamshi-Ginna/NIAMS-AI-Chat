ccba515c85648a5324914f2279a30e4a06124ebf
Vamshi Ginna <vamshi.ginna@lcginc.com>
2024-09-16 13:28:39 -0400

    Finalized side bar

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,19 +1,50 @@
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import {FaAngleLeft , FaBars, FaInfoCircle, FaComments, FaTimes, FaTrash, FaPlus, FaCommentDots } from 'react-icons/fa';
-import { IoDocumentTextSharp  } from "react-icons/io5";
-import { v4 as uuidv4 } from 'uuid';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  FaDollarSign,
+  FaCoins,
+  FaUserCircle,
+  FaAngleLeft,
+  FaBars,
+  FaInfoCircle,
+  FaComments,
+  FaTimes,
+  FaTrash,
+  FaPlus,
+  FaCommentDots,
+} from "react-icons/fa";
+import { IoDocumentTextSharp } from "react-icons/io5";
+import { v4 as uuidv4 } from "uuid";
+import { TbMessageFilled } from "react-icons/tb";
 
 interface SidebarProps {
-  chats: { id: string; name: string; messages: { type: string; content: string }[]; tokens: number; cost: number; showPrompts: boolean }[];
-  setChats: React.Dispatch<React.SetStateAction<{ id: string; name: string; messages: { type: string; content: string }[]; tokens: number; cost: number; showPrompts: boolean }[]>>;
+  chats: {
+    id: string;
+    name: string;
+    messages: { type: string; content: string }[];
+    tokens: number;
+    cost: number;
+    showPrompts: boolean;
+  }[];
+  setChats: React.Dispatch<
+    React.SetStateAction<
+      {
+        id: string;
+        name: string;
+        messages: { type: string; content: string }[];
+        tokens: number;
+        cost: number;
+        showPrompts: boolean;
+      }[]
+    >
+  >;
   userName: string;
 }
 
 const Sidebar: React.FC<SidebarProps> = ({ chats, setChats, userName }) => {
   const [isCollapsed, setIsCollapsed] = useState(false);
   const [editingChatId, setEditingChatId] = useState<string | null>(null);
-  const [newChatName, setNewChatName] = useState<string>('');
+  const [newChatName, setNewChatName] = useState<string>("");
   const [showTooltip, setShowTooltip] = useState<string | null>(null);
   const navigate = useNavigate();
 
@@ -44,85 +75,140 @@ const Sidebar: React.FC<SidebarProps> = ({ chats, setChats, userName }) => {
   };
 
   const handleDeleteChat = (chatId: string) => {
-    if (window.confirm('Are you sure you want to delete this chat?')) {
+    if (window.confirm("Are you sure you want to delete this chat?")) {
       setChats(chats.filter((chat) => chat.id !== chatId));
-      navigate('/');
+      navigate("/");
     }
   };
 
   const handleSaveChatName = (chatId: string) => {
-    if (!newChatName.trim()) {
+    if (newChatName.trim()) {
+      setChats(
+        chats.map((chat) =>
+          chat.id === chatId ? { ...chat, name: `${newChatName}` } : chat
+        )
+      );
+      setNewChatName("");
       setEditingChatId(null);
-      return;
     }
-    setChats(chats.map((chat) => (chat.id === chatId ? { ...chat, name: newChatName } : chat)));
-    setEditingChatId(null);
   };
 
   const handleBlur = (chatId: string, currentChatName: string) => {
-    if (!newChatName.trim()) {
-      setNewChatName(currentChatName);
-    } else {
+    if (newChatName.trim()) {
       handleSaveChatName(chatId);
+    } else {
+      setNewChatName(currentChatName);
     }
     setEditingChatId(null);
   };
 
   return (
-    <div className={`bg-purple-800 shadow-xl ${isCollapsed ? 'w-16' : 'w-64'} flex flex-col h-screen max-h-screen relative transition-all duration-300 ease-in-out`}>
+    <div
+      className={`bg-pinkish shadow-xl ${isCollapsed ? "w-16" : "w-64"} flex flex-col h-screen max-h-screen relative transition-all duration-300 ease-in-out`}
+    >
       {/* Top Section: Collapse Button & Logo */}
-      <div className="p-4 flex items-center"> 
-        <button onClick={toggleSidebar} className="text-white hover:text-purple-300 transition-colors duration-300">
-          {isCollapsed ? <FaBars /> : <FaAngleLeft  />}
-        </button>
+      <div className="p-4 flex items-center">
         {!isCollapsed && (
-          <div className="flex items-center ml-1"> {/* Adjusted margin-left to 'ml-1' for a smaller gap */}
-            <img src="/niams_white_logo.png" alt="Logo" className="h-11 w-auto" /> {/* Kept the reduced size */}
+          <div className="bg-white p-2 rounded-lg shadow-md">
+            <img
+              src="/niams_logo.jpg"
+              alt="Logo"
+              className="h-12 w-auto mx-auto"
+            />
           </div>
         )}
       </div>
+      <hr className="border-t-1 border-pinkish_dark" />
+      <div className="p-4 flex items-center">
+        <button
+          onClick={toggleSidebar}
+          aria-label="Toggle sidebar"
+          className="text-black hover:text-white transition-colors duration-300"
+        >
+          {isCollapsed ? <FaBars /> : <FaAngleLeft />}
+        </button>
+        {/* User Info */}
+        {!isCollapsed && (
+          <div className="pl-4 flex items-center text-black">
+            <div className="flex items-center">
+              {/* User Icon */}
+              <div className="mr-3">
+                <FaUserCircle className="text-4xl text-black" />
+              </div>
+              <div>
+                {/* User Name */}
+                <span className="block font-semibold text-sm">
+                  Logged in as:
+                </span>
+                <span className="block font-semibold text-base">
+                  {userName}
+                </span>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+      <hr className="border-t-1 border-pinkish_dark" />
       {/* Navigation */}
       <nav className="my-6 ">
         <div className="flex flex-col">
-          <Link to="/" className="text-white font-semibold hover:bg-purple-700 flex items-center py-3 px-4 rounded-md transition-colors duration-300">
-            <FaInfoCircle className="mr-2" />
+          <Link
+            to="/"
+            className="text-black font-semibold hover:bg-pinkish_dark hover:text-white flex items-center py-3 px-4 rounded-md transition-colors duration-300"
+          >
+            <FaInfoCircle className="mr-2" aria-label="Overview" />
             {!isCollapsed && <span>Overview</span>}
           </Link>
-          <Link to="/training" className="text-white font-semibold hover:bg-purple-700 flex items-center py-3 px-4 rounded-md transition-colors duration-300">
-            <IoDocumentTextSharp  className="mr-2" />
+          <Link
+            to="/training"
+            className="text-black font-semibold hover:bg-pinkish_dark hover:text-white flex items-center py-3 px-4 rounded-md transition-colors duration-300"
+          >
+            <IoDocumentTextSharp className="mr-2" aria-label="Training" />
             {!isCollapsed && <span>Training</span>}
           </Link>
 
-          <div onClick={handleChatClick} className="text-white font-semibold hover:bg-purple-700 flex items-center py-3 px-4 cursor-pointer rounded-md transition-colors duration-300">
-            <FaComments className="mr-2" />
+          {/* <div onClick={handleChatClick} className="text-white font-semibold hover:bg-purple-700 flex items-center py-3 px-4 cursor-pointer rounded-md transition-colors duration-300">
+            <FaComments className="mr-2" aria-label="Chat" />
             {!isCollapsed && <span>Chat</span>}
-          </div>
+          </div> */}
 
-          <div onClick={handleNewChat} className="text-white font-semibold hover:bg-purple-700 flex items-center py-3 px-4 cursor-pointer rounded-md transition-colors duration-300">
-            <FaPlus className="mr-2" />
+          <div
+            onClick={handleNewChat}
+            className="text-black font-semibold hover:bg-pinkish_dark hover:text-white flex items-center py-3 px-4 cursor-pointer rounded-md transition-colors duration-300"
+          >
+            <FaPlus className="mr-2" aria-label="Start New Chat" />
             {!isCollapsed && <span>Start New Chat</span>}
           </div>
         </div>
       </nav>
 
-      <hr/>
+      <hr className="border-t-1 border-pinkish_dark" />
 
-      {/* User Info */}
+      {/* Token Info */}
       {!isCollapsed && (
-        <div className="mt-4 text-white pl-4">
-          <span className="block font-semibold text-sm">Logged in as: {userName}</span>
-          <span className="block font-semibold text-sm mt-2">Total Tokens: {totalTokens}</span>
-          <span className="block font-semibold text-sm">Total Cost: ${totalCost.toFixed(2)}</span>
+        <div className="text-black pl-4">
+          <div className="flex items-center py-2">
+            <FaCoins className="mr-2 text-xl" /> {/* Total Tokens Icon */}
+            <span className="font-semibold text-sm">
+              Total Tokens: {totalTokens}
+            </span>
+          </div>
+          <div className="flex items-center py-2">
+            <FaDollarSign className="mr-2 text-xl" /> {/* Total Cost Icon */}
+            <span className="font-semibold text-sm">
+              Total Cost: ${totalCost.toFixed(2)}
+            </span>
+          </div>
         </div>
       )}
-
+      <hr className="border-t-1 border-pinkish_dark" />
       {/* Chat List */}
       {!isCollapsed && (
         <div className="p-4 overflow-auto flex-1">
           {chats.map((chat) => (
             <div
               key={chat.id}
-              className="relative flex justify-between items-center group hover:bg-purple-700 text-white rounded-md transition-colors duration-300"
+              className="relative flex justify-between items-center group hover:bg-pinkish_dark hover:text-white text-black rounded-md transition-colors duration-300"
               onMouseEnter={() => setShowTooltip(chat.id)}
               onMouseLeave={() => setShowTooltip(null)}
             >
@@ -136,29 +222,33 @@ const Sidebar: React.FC<SidebarProps> = ({ chats, setChats, userName }) => {
                 />
               ) : (
                 <div
-                className="relative z-20 font-semibold block py-2 px-4 pointer-events-none flex items-center"
-                onDoubleClick={() => {
-                  setEditingChatId(chat.id);
-                  setNewChatName(chat.name);
-                }}
-                style={{
-                  overflow: 'hidden',
-                  whiteSpace: 'nowrap',
-                  textOverflow: 'ellipsis',
-                  maxWidth: '22ch',
-                }}
-              >
-                <FaCommentDots className="mr-2 text-white-400" />
-                <Link to={`/chat/${chat.id}`} className="pointer-events-auto" title={chat.name}>
-                  {chat.name.length > 16 ? `${chat.name.substring(0, 16)}...` : chat.name}
-                </Link>
-              </div>
-              
+                  className="relative z-20 font-semibold block py-2 px-4 pointer-events-none flex items-center"
+                  onDoubleClick={() => {
+                    setEditingChatId(chat.id);
+                    setNewChatName(chat.name);
+                  }}
+                  style={{
+                    overflow: "hidden",
+                    whiteSpace: "nowrap",
+                    textOverflow: "ellipsis",
+                    maxWidth: "22ch",
+                  }}
+                >
+                  <TbMessageFilled className="mr-2 text-black-400" />
+                  <Link
+                    to={`/chat/${chat.id}`}
+                    className="pointer-events-auto"
+                    title={chat.name}
+                  >
+                    {chat.name.length > 14
+                      ? `${chat.name.substring(0, 14)}...`
+                      : chat.name}
+                  </Link>
+                </div>
               )}
 
               {showTooltip === chat.id && (
-                <div className="absolute top-full mt-1 bg-gray-700 text-white text-xs rounded-lg py-1 px-2 shadow-lg"
-                >
+                <div className="absolute top-full mt-1 bg-gray-700 text-white text-xs rounded-lg py-1 px-2 shadow-lg z-50">
                   Double-click to rename
                 </div>
               )}
@@ -168,7 +258,8 @@ const Sidebar: React.FC<SidebarProps> = ({ chats, setChats, userName }) => {
                   e.stopPropagation();
                   handleDeleteChat(chat.id);
                 }}
-                className="relative z-20 text-white-300 hover:text-red-500 p-1 transition-colors duration-300"
+                className="relative z-20 text-black hover:text-red-500 p-1 transition-colors duration-300"
+                aria-label="Delete Chat"
               >
                 <FaTrash />
               </button>
